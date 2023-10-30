c44437b7cda87d5e240b9821f8563746f187d101
jackoozy <jmccannprojects@gmail.com>
2023-10-30 12:15:59 +1000

    initial commit.

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/jackmccann/fetch_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/jackmccann/test_ws/devel;/home/jackmccann/fetch_ws/devel;/home/jackmccann/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/jackmccann/test_ws/devel;/home/jackmccann/fetch_ws/devel;/home/jackmccann/catkin_ws/devel;/opt/ros/melodic' != '' else []
